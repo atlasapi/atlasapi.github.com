446244249e96ad3193a35667e250deb0c1f83839
Luke Wesley-Holley <luke@lukewh.com>
2011-03-01 15:45:13 +0000

    Various updates.

--- dev/script.js
+++ dev/script.js
@@ -63,13 +63,13 @@ PageInfo.prototype.init = function() {
     });
     pageInfo.sections = pageInfo.section.length-1;
 
-    $.history.init(function(hash){
+   /*$.history.init(function(hash){
         if(hash == "") {
         } else {
             pageInfo.update(false,false,true);
         }
     },
-    { unescape: ",/" });
+    { unescape: ",/" });*/
 }
 
 PageInfo.prototype.update = function(width,height,offset) {
@@ -100,7 +100,7 @@ PageInfo.prototype.update = function(width,height,offset) {
 
 PageInfo.prototype.changePage = function(i) {
     var pageInfo = this;
-    var homeDemo = new HomeDemo();
+    //var homeDemo = new HomeDemo();
     $('header a.selected').removeClass('selected');
     $('header a[href="#'+pageInfo.section[i].name+'"]').addClass('selected');
     // Change hash
@@ -117,7 +117,8 @@ PageInfo.prototype.changeHash = function(n) {
 
 var HomeDemo = function(item) {
     this.active = false;
-    this.query = [
+    
+    var array0 = [
         'discover.json?publisher=bbc.co.uk&genre=http://ref.atlasapi.org/genres/atlas/drama',
         'discover.json?publisher=bbc.co.uk&available=true',
         'discover.json?publisher=bbc.co.uk&availableCountries=uk',
@@ -128,29 +129,41 @@ var HomeDemo = function(item) {
         'discover.json?publisher=bbc.co.uk&genre=http://ref.atlasapi.org/genres/atlas/drama',
         'discover.json?publisher=bbc.co.uk&genre=http://ref.atlasapi.org/genres/atlas/drama',
         'discover.json?publisher=bbc.co.uk&genre=http://ref.atlasapi.org/genres/atlas/drama'
+        /* '1','2','3','4','5','6','7','8','9','10' */
     ];
-    this.activeQuery;
+    // Select Random Query to start on
+    var randomQuery = Math.floor(Math.random()*array0.length);
+    
+    // Shift the query order
+    var array1 = array0.slice(0,randomQuery);
+    var array2 = array0.slice(randomQuery);
+    this.query = array2.concat(array1);
+    
+    this.activeQuery = 0;
+    this.marker = 0;
+    
     this.time = 20000;
     this.timer;
     
     this.nav = item;
-    this.currentPosition = 0;
     this.item = [];
     this.width = 0;
+    
+    this.prevBtn = item.find('.cbtn.previous');
+    this.nextBtn = item.find('.cbtn.next');
 }
 
 HomeDemo.prototype.init = function(){
     var homeDemo = this;
     
-    // Select Random Query to start on
-    homeDemo.activeQuery = Math.floor(Math.random()*homeDemo.query.length);
-    
     // Add queries to UL
+    console.log('add queries to dom');
     $.each(homeDemo.query, function(i){
-        $('.controlBar .queryHolder .queries').append('<li><a href="#">'+homeDemo.query[i]+'</a></li>');
+        $('.controlBar .queryHolder .queries').append('<li><a href="#" class="api">'+homeDemo.query[i]+'</a></li>');
     });
     
     // Add each query element to array
+    console.log('create array of said elements');
     $('.controlBar .queryHolder .queries li').each(function(i){
         /*homeDemo.width += $(this).outerWidth(true);
         console.log('+'+$(this).outerWidth(true));*/
@@ -158,6 +171,7 @@ HomeDemo.prototype.init = function(){
     });
     
     // Add current class to first query
+    console.log('add current class to first item');
     homeDemo.item[0].item.addClass('current');
     
     // Set width of scroller
@@ -167,18 +181,46 @@ HomeDemo.prototype.init = function(){
     // Add onclick event to buttons
     homeDemo.nav.find('.cbtn').click(function(){
     	if(!$(this).hasClass('inactive')) {
+    	   console.log('home demo marker='+homeDemo.marker);
 			if($(this).hasClass('previous')){
 				clearInterval(homeDemo.timer);
-				homeDemo.currentPosition++;
-				homeDemo.scrollIt(homeDemo.currentPosition,0);
+				homeDemo.activeQuery++;
+				if(homeDemo.activeQuery == 10) {
+				    homeDemo.activeQuery = 0;
+				}
+				console.log(homeDemo.activeQuery);
+				if(homeDemo.activeQuery > homeDemo.marker && homeDemo.nextBtn.hasClass('wait')) {
+				    console.log('Active query is above 0');
+				    homeDemo.nextBtn.removeClass('inactive').removeClass('wait').find('.icn').css('width',16);
+				    homeDemo.nextBtn.find('.js_txt').html('Next');
+				}
+				if(homeDemo.activeQuery >= homeDemo.marker){
+				    console.log('greater');
+				    homeDemo.nextQuery();
+				    if(homeDemo.activeQuery == homeDemo.marker) {
+                        homeDemo.nextBtn.addClass('inactive').addClass('wait');
+	       				homeDemo.countdown();
+				    }
+                } else if(homeDemo.activeQuery < homeDemo.marker) {
+                    console.log('less');
+                    homeDemo.nextQuery();
+                }
 			} else if($(this).hasClass('next')) {
 				if(!$(this).hasClass('wait')){
 					clearInterval(homeDemo.timer);
-					if(homeDemo.currentPosition != 0){
-						homeDemo.currentPosition--;
-						homeDemo.scrollIt(homeDemo.currentPosition,1);
-					} else {
-						homeDemo.countdown();
+					homeDemo.activeQuery--;
+					if(homeDemo.activeQuery == -1) {
+					   homeDemo.activeQuery = 9;
+					}
+					console.log(homeDemo.activeQuery);
+					if(homeDemo.activeQuery >= homeDemo.marker){
+					   homeDemo.prevQuery();
+					   if(homeDemo.activeQuery == homeDemo.marker) {
+    					   homeDemo.nextBtn.addClass('inactive').addClass('wait');
+	       				   homeDemo.countdown();
+	       				}
+					} else if(homeDemo.activeQuery > homeDemo.marker){
+					   homeDemo.prevQuery();
 					}
 				}
 			}
@@ -187,39 +229,19 @@ HomeDemo.prototype.init = function(){
     });
     
     // Make first request
+    console.log('make first request');
     homeDemo.request();
-}
-
-HomeDemo.prototype.scrollIt = function(pos,mod) {
-    var homeDemo = this;
-    console.log(pos);
-    homeDemo.countdown();
-    if(mod==0){
-    	if(pos > 0 && pos < 10) {
-            homeDemo.nav.find('.queries').animate({
-                right: -775*pos
-            },500);
-            homeDemo.item[pos-1].item.removeClass('current');
-	    }
-	    if(pos == 9) {
-	    	homeDemo.nav.find('.previous').addClass('inactive');
-	    }
-	} else {
-		homeDemo.nav.find('.queries').animate({
-			right: -775*pos
-		},500);
-		homeDemo.item[pos+1].item.removeClass('current');
-	}
-
-    homeDemo.item[pos].item.addClass('current');
 }
 
 HomeDemo.prototype.request = function() {
     var homeDemo = this;
     // Clear timeout
+    console.log('clear timeout');
     clearTimeout(homeDemo.timer);
     
-    console.log(homeDemo.query[homeDemo.activeQuery]);
+    homeDemo.prevBtn.addClass('inactive');
+    
+    console.log('active query='+homeDemo.activeQuery);
     // Make request
     $.ajax({
         url: 'http://otter.atlasapi.org/3.0/'+homeDemo.query[homeDemo.activeQuery]+'&limit=2',
@@ -238,15 +260,48 @@ HomeDemo.prototype.request = function() {
         complete: function(jqXHR, textStatus){
             console.log(textStatus);
             // Set activeQuery to next in list
-            if(homeDemo.activeQuery < homeDemo.query.length) {
+            if(homeDemo.activeQuery < homeDemo.query.length-1) {
                 homeDemo.activeQuery++;
+                homeDemo.marker++;
+                console.log('new active query='+homeDemo.activeQuery);
             } else {
+                console.log('new active query=0');
                 homeDemo.activeQuery = 0;
+                homeDemo.marker = 0;
             }
+            
+            // Scroll left 1. Remove the item from the list and add it to the end
+            console.log('next query');
+            homeDemo.nextQuery();
             // Restart Countdown
             homeDemo.countdown();
+            homeDemo.prevBtn.removeClass('inactive');
         }
     });
+}
+
+HomeDemo.prototype.nextQuery = function() {
+    var homeDemo = this;
+    
+    if(homeDemo.activeQuery == 0){
+        var item = homeDemo.item[9].item;
+    } else {
+        var item = homeDemo.item[homeDemo.activeQuery-1].item;
+    }
+    
+    item.removeClass('current').detach().appendTo('.queries');
+    homeDemo.item[homeDemo.activeQuery].item.addClass('current');
+}
+
+HomeDemo.prototype.prevQuery = function() {
+    var homeDemo = this;
+    if(homeDemo.activeQuery == 9){
+        var item = homeDemo.item[0].item;
+    } else {
+        var item = homeDemo.item[homeDemo.activeQuery+1].item;
+    }
+    item.removeClass('current');
+    homeDemo.item[homeDemo.activeQuery].item.addClass('current').detach().prependTo('.queries');
 }
 
 HomeDemo.prototype.countdown = function() {
@@ -254,22 +309,34 @@ HomeDemo.prototype.countdown = function() {
     clearInterval(homeDemo.timer); 
 	var i = 20;
 	var thisItem = homeDemo.nav.find('.js_txt');
-	if(homeDemo.currentPosition == 0) {
-		if(!thisItem.hasClass('inactive')){
-			thisItem.parent('a').addClass('inactive');
-		}
-		homeDemo.timer = setInterval(function() {
-			thisItem.html(i+'s').siblings('.icn').css('width','16px').parent('a').addClass('wait');
+	if(homeDemo.buttonClicked) {
+    	if(homeDemo.activeQuery == 0) {
+    		if(!thisItem.hasClass('inactive')){
+    			thisItem.parent('a').addClass('inactive');
+    		}
+    		homeDemo.timer = setInterval(function() {
+    			thisItem.html(i+'s').siblings('.icn').css('width','16px').parent('a').addClass('wait');
+    			i--;
+    			if(i== -1){
+    				thisItem.html('Loading').siblings('.icn').css('width','0');
+    				clearTimer(homeDemo.timer);
+    				homeDemo.request();
+    			}
+    		}, 1000);
+    	} else {
+    		thisItem.html('Next').siblings('.icn').css('width','16px').parent('a').removeClass('wait').removeClass('inactive');
+    	}
+    } else {
+        homeDemo.timer = setInterval(function() {
+            thisItem.html(i+'s').siblings('.icn').css('width','16px').parent('a').addClass('wait');
 			i--;
 			if(i== -1){
 				thisItem.html('Loading').siblings('.icn').css('width','0');
 				clearTimer(homeDemo.timer);
 				homeDemo.request();
 			}
-		}, 1000);
-	} else {
-		thisItem.html('Next').siblings('.icn').css('width','16px').parent('a').removeClass('wait').removeClass('inactive');
-	}
+        }, 1000);
+    }
 }
 
 
@@ -279,12 +346,12 @@ $(document).ready(function(){
     
     $('section:last-child').css('min-height', pageInfo.pageHeight);
     
+    var homeDemo = new HomeDemo($('.controlBar'));
+    homeDemo.init();
+    
     var tabs = new Tabs();
     tabs.init($('#explorerWrapper'));
     tabs.changeTab(0);
-    
-    var homeDemo = new HomeDemo($('.controlBar'));
-    homeDemo.init();
     
     
     $('input.date').datepicker({
