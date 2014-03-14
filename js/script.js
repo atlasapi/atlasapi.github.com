754e11166ba589d624fbc058052075bc52caacee
Lukewh <luke@lukewh.com>
2014-03-14 14:31:32 +0000

    Changed wording for login

--- js/script.js
+++ js/script.js
@@ -1916,7 +1916,7 @@ $(document).ready(function(){
 	    $.ajax({
 		    url: 'http://atlas.metabroadcast.com/4.0/auth/providers.json',
 			success: function (data) {
-				var str = '<div style="font-size: 16px; text-align: left;" class="mbl">Login using oauth with one of the services below</div>',
+				var str = '<div style="font-size: 16px; text-align: left;" class="mbl">Log in with one of the services below</div>',
 					i,
 					ii;
 				
