3fef6d62ec23f96cf4f351992b3ec205bb6e61e5
Steve Rydz <steverydz@gmail.com>
2014-09-12 14:09:31 +0100

    Removed framework and added gulp task to download it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
 var gulp = require('gulp'),
+
+    frameworkURL = 'http://assets.mbst.tv/scss/framework/',
+    frameworkVersion = '0.0.15',
+    frameworkPath = './css/scss/framework',
+
     sass = require('gulp-ruby-sass'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    remoteSrc = require('gulp-remote-src'),
+    unzip = require('gulp-unzip'),
+    clean = require('gulp-rimraf'),
+
+    // Paths
+    paths = {
+      dist: 'css/'
+    };
+
+gulp.task('getFramework', function () {
+  return remoteSrc('archive.zip', {
+    base: frameworkURL + frameworkVersion + '/'
+  })
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('unzipFramework', ['getFramework'], function () {
+  gulp.src('./archive.zip')
+    .pipe(unzip())
+    .pipe(gulp.dest(frameworkPath + '/'));
+});
+
+gulp.task('deleteZipFile', ['unzipFramework'], function () {
+  return gulp.src('./archive.zip', {read: false})
+    .pipe(clean({force: true}));
+});
 
 gulp.task('sass', function () {
   gulp.src('css/scss/main.scss')
@@ -16,4 +47,4 @@ gulp.task('watch', function () {
   gulp.watch('css/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['getFramework', 'unzipFramework', 'deleteZipFile', 'sass']);
