525a23a787ff15c580cf766a92ee1a0f3fe21caf
Steve Rydz <steverydz@gmail.com>
2014-11-27 11:07:24 +0000

    Add form to put in api key

--- js/api-explorer/tabs.js
+++ js/api-explorer/tabs.js
@@ -3,14 +3,14 @@ var app = app || {};
 app.tabs = function (tabSection) {
   $(tabSection).each(function () {
     var $tabs = $(tabSection);
-    $(this).find(".tab-panel").not(":first").hide();
-    $tabs.find(".tabs li:first-child").addClass("tabs-current");
+    $(this).find(".tabArea").not(":first").hide();
+    $tabs.find(".tabs li:first-child").addClass("selected");
     $tabs.find(".tabs a").on("click", function (e) {
       e.preventDefault();
       var $this = $(this);
       var tabTarget = $this.attr("href");
       $this.parent().addClass("selected").siblings().removeClass("selected");
-      $(tabTarget).show().siblings(".tab-panel").hide();
+      $(tabTarget).show().siblings(".tabArea").hide();
     });
   });
 };
