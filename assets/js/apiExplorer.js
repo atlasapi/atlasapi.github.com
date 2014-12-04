1edf427c370b3cc803dcfcc75ffa958d6235607f
Steve Rydz <steverydz@gmail.com>
2014-12-04 14:40:56 +0000

    Combine templates into one file

--- assets/js/apiExplorer.js
+++ assets/js/apiExplorer.js
@@ -5,19 +5,13 @@ var ApiExplorer = function () {
   this.endpointsParametersUrl = 'assets/data/parameters.json';
   this.apiKey = 'c1e92985ec124202b7f07140bcde6e3f';
   this.queryUrl = '//atlas.metabroadcast.com';
-  this.templates = [
-    {
-      path: 'assets/templates/navigation.ejs',
-      container: '#apiExplorerNavigation'
-    },
-    {
-      path: 'assets/templates/sections.ejs',
-      container: '#apiExplorerTabSections'
-    }
-  ];
+  this.template = {
+    path: 'assets/templates/apiExplorer.ejs',
+    container: '#apiExplorerTabSections'
+  };
 };
 
-ApiExplorer.prototype.setApiKey = function (apiKey, data) {
+ApiExplorer.prototype.setApiKey = function (data) {
   'use strict';
 
   var apiExplorer = this,
@@ -38,13 +32,46 @@ ApiExplorer.prototype.setApiKey = function (apiKey, data) {
       apiExplorer.buildQueryUrl(data[i]);
     }
 
-    for (var j = 0, jj = apiExplorer.templates.length; j < jj; j++) {
-      apiExplorer.compileTemplate(data, apiExplorer.templates[j].path, apiExplorer.templates[j].container);
-    }
+    apiExplorer.compileTemplate(data, apiExplorer.template.path, apiExplorer.template.container);
   });
 
   $apiKeyForm.on('submit', function (e) {
     e.preventDefault();
+  });
+};
+
+ApiExplorer.prototype.setParameters = function (form, data) {
+  'use strict';
+
+  var apiExplorer = this,
+      $queryParametersForm = $(form),
+      $parameters = $queryParametersForm.find('.queryParameter'),
+      endpoints = data;
+
+  $parameters.each(function () {
+    $(this).on('change', function () {
+      var $this = $(this),
+          newValue = $this.val(),
+          endpointName = $this.data('endpoint'),
+          parameterName = $this.attr('name');
+
+      for (var i = 0, ii = endpoints.length; i < ii; i++) {
+        if (endpoints[i].name === endpointName) {
+          for (var j = 0, jj = endpoints[i].parameters.length; j < jj; j++) {
+            if (endpoints[i].parameters[j].name === parameterName) {
+              endpoints[i].parameters[j].default_value = newValue;
+            }
+          }
+        }
+        apiExplorer.buildQueryUrl(endpoints[i]);
+      }
+
+      for (var k = 0, kk = endpoints.length; k < kk; k++) {
+        apiExplorer.buildQueryUrl(endpoints[k]);
+      }
+
+      apiExplorer.compileTemplate(endpoints, apiExplorer.template.path, apiExplorer.template.container);
+    });
   });
 };
 
@@ -152,10 +179,8 @@ ApiExplorer.prototype.init = function () {
   var apiExplorer = this,
       data = apiExplorer.mergeData();
 
-  for (var i = 0, ii = apiExplorer.templates.length; i < ii; i++) {
-    apiExplorer.compileTemplate(data, apiExplorer.templates[i].path, apiExplorer.templates[i].container);
-  }
-
-  apiExplorer.setApiKey(apiExplorer.apiKey, data);
+  apiExplorer.compileTemplate(data, apiExplorer.template.path, apiExplorer.template.container);
+  apiExplorer.setApiKey(data);
+  apiExplorer.setParameters('#queryParametersForm', data);
   apiExplorer.submitQueryForm();
 };
