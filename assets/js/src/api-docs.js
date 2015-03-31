3ef502850b30376df413f34e0dd625e48930b9f5
Steve Rydz <steverydz@gmail.com>
2015-03-31 11:56:12 +0100

    Make side nav into reusable component

--- assets/js/src/api-docs.js
+++ assets/js/src/api-docs.js
@@ -1,10 +1,10 @@
 $(function () {
-  $('.api-docs-nav-item a').each(function (index, link) {
+  $('.api-docs-nav .menu-item').each(function (index, link) {
     $(link).on('click', function (e) {
       e.preventDefault();
       var panelId = $(link).attr('href');
-      $('.api-docs-nav-item a').removeClass('active-panel');
-      $(link).addClass('active-panel');
+      $('.api-docs-nav .menu-item').removeClass('menu-item-selected');
+      $(link).addClass('menu-item-selected');
       $('.api-docs-panel').hide();
       $(panelId).show();
     });
