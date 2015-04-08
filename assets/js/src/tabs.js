9ed4e098158fe696d4f513bd1cd59a9accce9f2b
Steve Rydz <steverydz@gmail.com>
2015-04-08 16:41:32 +0100

    Tabs now show correctly if URL contains a hash

--- assets/js/src/tabs.js
+++ assets/js/src/tabs.js
@@ -3,30 +3,29 @@ var UiTabs = (function () {
 
   var showActiveTab = function () {
     var activeClass = 'ui-tabs-active-link';
+    var active = location.hash;
+
+    var transition = function (selector, link) {
+      activateTab(selector);
+      activateLink(link);
+    };
+
+    var activateLink = function (elem) {
+      $(elem).closest('.ui-tabs-nav').find('.' + activeClass).removeClass(activeClass);
+      $(elem).addClass(activeClass);
+    };
+
+    var activateTab = function (tabSelector) {
+      $(tabSelector).siblings('.ui-tabs-panel').hide();
+      $(tabSelector).show();
+    };
+
+    if (active) {
+      transition(active, 'a[href="' + active + '"]');
+    }
 
     $('.ui-tabs').each(function () {
       var $tabsContainer = $(this);
-
-      var activateLink = function (elem) {
-        $tabsContainer.find('.' + activeClass).removeClass(activeClass);
-        $(elem).addClass(activeClass);
-      };
-
-      var activateTab = function (tabSelector) {
-        $tabsContainer.find('.ui-tabs-panel').hide();
-        $(tabSelector).show();
-      };
-
-      var transition = function (selector, link) {
-        activateTab(selector);
-        activateLink(link);
-      };
-
-      var active = location.hash;
-
-      if (active) {
-        transition(active, 'a[href="' + active + '"]');
-      }
 
       $tabsContainer.find('.ui-tabs-link').on('click', function (e) {
         e.preventDefault();
