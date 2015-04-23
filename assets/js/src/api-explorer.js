41a74ed5ba5a6f1fdef2a804e90c0eb02d3d7b5a
Steve Rydz <steverydz@gmail.com>
2015-04-23 10:59:31 +0100

    Move api key warning to all examples

--- assets/js/src/api-explorer.js
+++ assets/js/src/api-explorer.js
@@ -195,8 +195,8 @@ var apiExplorer = (function () {
     $(document).on('click', '.logout', function () {
       loadApiKeyButton();
     });
-    $(document).on('focus', '.has-api-key-warning', function () {
-      var $apiKeyWarning = $(this).closest('.queryForm').find('.api-key-warning');
+    $(document).on('click', '.show-api-key-warning', function () {
+      var $apiKeyWarning = $(this).closest('.has-api-key-warning').find('.api-key-warning');
       $('.api-key-warning').hide();
       $apiKeyWarning.show();
     });
