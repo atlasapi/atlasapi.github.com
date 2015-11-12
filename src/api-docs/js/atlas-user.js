e311d352c46cf95c78ab999bbb1c0abc1634cbee
Steve Rydz <steverydz@gmail.com>
2015-11-12 11:04:56 +0000

    Move api explorer files

---
+++ src/api-docs/js/atlas-user.js
@@ -0,0 +1,32 @@
+var atlasUser = (function () {
+  'use strict';
+  
+  var isLoggedIn = function () {
+    return localStorage.getItem('auth.token') ? true : false;
+  };
+
+  var getCredentials = function () {
+    return {
+      oauth_provider: localStorage.getItem('auth.provider'),
+      oauth_token: localStorage.getItem('auth.token')
+    };
+  };
+
+  var getUserData = function (url, callback) {
+    $.ajax({
+      url: url,
+      success: function (data) {
+        callback(data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        log.error(errorThrown);
+      }
+    });
+  };
+  
+  return {
+    isLoggedIn: isLoggedIn,
+    getCredentials: getCredentials,
+    getUserData: getUserData
+  };
+})();
